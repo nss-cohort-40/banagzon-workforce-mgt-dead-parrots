b58b2b5b72ed7a374652d1a815d5f46d1b9b30d3
Davis Lindell <daviselindell@gmail.com>
2020-08-11 13:55:46 -0500

    wip

--- hrapp/views/computers/form.py
+++ hrapp/views/computers/form.py
@@ -3,6 +3,16 @@ from django.shortcuts import render, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from ..connection import Connection
 
+
+def get_employees():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        
+        """)
+
 def get_computers():
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = sqlite3.Row
