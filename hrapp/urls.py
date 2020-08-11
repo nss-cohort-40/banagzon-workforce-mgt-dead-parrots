541bb540d98067a0b3a16f746b9fd968d2caec26
Ronald Lankford <rolankford@gmail.com>
2020-08-11 10:56:10 -0500

    added create tp link

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
-    path('employees/', employee_list, name='employee_list'),
+    path('employees/', home, name='employee_list'),
     path('departments/', home, name='department_list'),
     path('computers/', home, name='computer_list'),
     path('training_programs/', training_program_list, name='training_programs'),
