045785249e0d2209d8f0c92338d054b5116b0745
Davis Lindell <daviselindell@gmail.com>
2020-08-10 21:41:59 -0500

    moved a couple things and added a button... Done for the night

--- hrapp/static/scripts/computers.js
+++ hrapp/static/scripts/computers.js
@@ -2,7 +2,7 @@ const infoDialog = document.querySelector(".infoDialog")
 const message = document.querySelector(".infoDialog__message")
 const closeDialog = document.querySelector(".closeDialog")
 
-document.querySelector(".computers").addEventListener("click", (evt) => {
+document.querySelector(".detail_computers").addEventListener("click", (evt) => {
     if (evt.target.id.startsWith("detail")) {
         const id = evt.target.id.split("--")[1]
         message.innerText = 'Are you sure you want to delete this computer? This is irreversable and could cost you your job.'
