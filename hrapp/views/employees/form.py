dd0088d1d17cce11e8186a25a3bfab151a89753e
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-08-11 14:57:23 -0500

    departments show up on list view and form

--- hrapp/views/employees/form.py
+++ hrapp/views/employees/form.py
@@ -2,9 +2,10 @@ import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from hrapp.models import Employee
+from hrapp.models import Department
 from ..connection import Connection
 
-def get_employees():
+def get_employees(employee_id):
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
@@ -15,20 +16,37 @@ def get_employees():
                 e.first_name,
                 e.last_name,
                 e.start_date,
-                e.is_supervisor
-                e.department
+                e.is_supervisor,
+                e.department_id,
             from hrapp_employee e
-            """)
+            WHERE e.id = ?
+            """, (employee_id,))
 
             return db_cursor.fetchall()
+
+def get_departments():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            d.id,
+            d.dept_name,
+            d.budget
+        from hrapp_department d
+        """)
+
+        return db_cursor.fetchall()
+
 
 @login_required
 def employee_form(request):
   if request.method == 'GET':
-    employees = get_employees()
+    deparments = get_departments()
     template = 'employees/form.html'
     context = {
-        'all_employees': employees
+        'all_departments': deparments
     }
 
     return render(request, template, context)
