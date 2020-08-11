1d756f32066ffea2e3655005bb9908f7c6f02418
Davis Lindell <daviselindell@gmail.com>
2020-08-11 10:04:52 -0500

    wip

--- hrapp/models/employee_computer.py
+++ hrapp/models/employee_computer.py
@@ -3,8 +3,6 @@ from django.db import models
 class EmployeeComputer(models.Model):
     """
     Creates the join table for the many to many relationship between computers and employees
-    Author: Joe Shep
-    methods: none
     """
 
     employee = models.ForeignKey("Employee", on_delete=models.CASCADE)
