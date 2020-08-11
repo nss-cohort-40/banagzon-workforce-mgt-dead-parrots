375b7035ec2b12fc68c012918d6b7270ac93adcc
Davis Lindell <daviselindell@gmail.com>
2020-08-10 20:50:22 -0500

    couple typos

--- hrapp/static/scripts/computers.js
+++ hrapp/static/scripts/computers.js
@@ -5,7 +5,7 @@ const closeDialog = document.querySelector(".closeDialog")
 document.querySelector(".computers").addEventListener("click", (evt) => {
     if (evt.target.id.startsWith("detail")) {
         const id = evt.target.id.split("--")[1]
-        message.innerText = 'Are you sure you want to delete this computer?'
+        message.innerText = 'Are you sure you want to delete this computer? This is irreversable and could cost you your job.'
         infoDialog.show()
     }
 })
