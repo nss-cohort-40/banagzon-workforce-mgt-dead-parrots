0d595cc1a907e384f42084288b1e5079553602b1
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-08-12 10:17:32 -0500

    employees can see departments

--- hrapp/views/employees/details.py
+++ hrapp/views/employees/details.py
@@ -7,24 +7,45 @@ from hrapp.models import Department
 from ..connection import Connection
 
 
-def get_employees(employee_id):
-        with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = sqlite3.Row
-            db_cursor = conn.cursor()
+def create_employee(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+    employee = Employee()
+    employee.id = _row['employee_id']
+    employee.first_name = _row['first_name']
+    employee.last_name = _row['last_name']
+    employee.start_date = _row['start_date']
+    employee.is_supervisor = _row['is_supervisor']
+    employee.department_id = _row['department_id']
+    department = Department()
+    department.dept_name = _row['dept_name']
+    department.id = _row['department_id']
+    employee.department = department
+    return employee
 
-            db_cursor.execute("""
+
+def get_employee(employee_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_employee
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
             select
                 e.id employee_id,
                 e.first_name,
                 e.last_name,
                 e.start_date,
                 e.is_supervisor,
-                e.department_id
+                e.department_id,
+                d.dept_name,
+                d.id as department_id
             from hrapp_employee e
+            JOIN hrapp_department d ON
+            e.department_id = d.id
             WHERE e.id = ?
             """, (employee_id,))
 
-            return db_cursor.fetchone()
+        return db_cursor.fetchone()
+
 
 def get_departments():
     with sqlite3.connect(Connection.db_path) as conn:
@@ -41,10 +62,15 @@ def get_departments():
 
         return db_cursor.fetchall()
 
+    # def get_computers():
+
+    # def get_employee_training_program():
+
+
 @login_required
 def employee_details(request, employee_id):
     if request.method == 'GET':
-        employee = get_employees(employee_id)
+        employee = get_employee(employee_id)
 
         template = 'employees/detail.html'
         context = {
