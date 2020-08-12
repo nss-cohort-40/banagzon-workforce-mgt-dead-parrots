570cf578cbf5aa161ced44c611253d48bb4c12ec
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-08-12 14:37:34 -0500

    everything displays without multiple training programs diplaying

--- hrapp/views/employees/details.py
+++ hrapp/views/employees/details.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from hrapp.models import Employee
 from hrapp.models import Department
+from hrapp.models import Computer
+from hrapp.models import Training_program
 from ..connection import Connection
 
 
@@ -16,10 +18,20 @@ def create_employee(cursor, row):
     employee.start_date = _row['start_date']
     employee.is_supervisor = _row['is_supervisor']
     employee.department_id = _row['department_id']
+
     department = Department()
     department.dept_name = _row['dept_name']
     department.id = _row['department_id']
-    employee.department = department
+
+    computer = Computer()
+    computer.make = _row['computer_make']
+    computer.id = _row['computer_id']
+    employee.computer = computer
+
+    training_program = Training_program()
+    training_program.name = _row['training_program_name']
+    training_program.id = _row['training_program_id']
+    employee.training_program = training_program
     return employee
 
 
@@ -37,11 +49,27 @@ def get_employee(employee_id):
                 e.is_supervisor,
                 e.department_id,
                 d.dept_name,
+                c.id as computer_id,
+                tp.id as training_program_id,
+                c.make as computer_make,
+                tp.name as training_program_name,
+                ec.id as employeecomputer_id,
+                tp.start_date as training_program_start,
+                tp.end_date as training_program_end,
+                etp.id as employeetrainingprogram_id,
                 d.id as department_id
             from hrapp_employee e
             JOIN hrapp_department d ON
             e.department_id = d.id
-            WHERE e.id = ?
+            JOIN hrapp_employeecomputer ec ON
+            e.id = ec.employee_id 
+            JOIN hrapp_employee_training_program etp ON
+            e.id = etp.employee_id
+            JOIN hrapp_computer c ON
+            c.id = ec.computer_id
+            JOIN hrapp_training_program tp ON
+            tp.id = etp.training_program_id
+            WHERE e.id = ?;
             """, (employee_id,))
 
         return db_cursor.fetchone()
