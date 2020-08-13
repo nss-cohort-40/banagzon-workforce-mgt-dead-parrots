fa9a0c336ffbe671129d5dca84d9b4184e299a3b
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-08-13 11:57:24 -0500

    delete works

--- hrapp/views/employees/details.py
+++ hrapp/views/employees/details.py
@@ -113,3 +113,20 @@ def employee_details(request, employee_id):
         }
 
         return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+    if (
+        "actual_method" in form_data
+        and form_data["actual_method"] == "DELETE"
+    ):
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            DELETE FROM hrapp_employee
+            WHERE id = ?
+            """, (employee_id,))
+
+        return redirect(reverse('hrapp:employee_list'))
